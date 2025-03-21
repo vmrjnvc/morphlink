2b7d80aec60e88ba2b9a7ddf8263a261fab954c2
vuk.marjanovic <vmrjnvc@gmail.com>
2025-03-21 16:57:38 +0100

    add and test open in new tab option

--- popup.js
+++ popup.js
@@ -5,3 +5,21 @@ document.querySelector('#go-to-options').addEventListener('click', function() {
         window.open(chrome.runtime.getURL('options.html'));
     }
 });
+
+// get current tab info from which extension is executed
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+document.querySelector('#open-new-tab').addEventListener('click', async function() {
+    const tab = await getCurrentTab();
+    console.log(tab);
+    chrome.tabs.create({
+        url: tab.url,
+    });
+});
+
+
