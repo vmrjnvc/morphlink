0bbfdceba4bdeb7ef001c794e82febae7cf82f67
vuk.marjanovic <vmrjnvc@gmail.com>
2025-05-07 17:25:57 +0200

    introduce vue framework

---
+++ components/Popup.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="popup">
+        <h1 class="title">Morphlink</h1>
+
+        <div class="container">
+            <div class="commands">
+                <div class="commands-section">
+                    <div class="commands-section__title">Commands</div>
+                    <div class="commands-section__title">Shortcuts</div>
+                </div>
+                <div class="commands-container"></div>
+                <div class="divider"></div>
+            </div>
+        </div>
+        <button class="options">Options</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
